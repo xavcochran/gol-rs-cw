a04cb7c8673e1ceaa5545e18da14c1f95571b1ff
0xFaded <xav@bracket.software>
2024-10-24 09:13:12 +0100

    implemented partial reads and writes for stream. Need to now test with single threaded worker

--- src/decode/packet.rs
+++ src/decode/packet.rs
@@ -3,11 +3,15 @@ use std::cell;
 
 use bytes::BytesMut;
 use indexmap::IndexSet;
+use std::result::Result::Ok;
 // use std::fmt::Error;
 // use std::time::Instant;
 // use std::{cell, fmt};
 // use std::{fs::read, vec};
-use tokio::{io::AsyncReadExt, net::TcpStream};
+use tokio::{
+    io::{AsyncReadExt, AsyncWriteExt},
+    net::TcpStream,
+};
 
 use super::{
     coordinates::Coordinates,
@@ -93,13 +97,14 @@ impl Packet {
 
     fn decode_payload(
         &mut self,
+        cells: &mut IndexSet<u32>,
         data: &[u8],
         coordinate_length: u32,
         offset: u32,
-    ) -> IndexSet<u32> {
+    ) {
         let mut buffer: u32 = 0;
         let mut bit_count = 7;
-        let mut cells = IndexSet::with_capacity(self.header.length as usize);
+
         // could borrow here
         let mask: u32 = self.generate_mask(coordinate_length);
         let limit = self.limit(coordinate_length);
@@ -119,53 +124,33 @@ impl Packet {
                 bit_count -= coordinate_length_usize; // decrease bit count to account for bits just extracted
             }
         }
-        return cells;
     }
 
-    pub async fn decode(&mut self, mut stream: TcpStream) -> Result<IndexSet<u32>, DecodeError> {
-        let mut buf = BytesMut::with_capacity(HEADER_SIZE_BYTES);
+    /// Reads data from TCP stream directly into set of alive cells
+    pub async fn read(&mut self, client: &mut TcpStream) -> Result<IndexSet<u32>, DecodeError> {
+        // initialses buffer for header read
+        let mut header_buf = BytesMut::with_capacity(HEADER_SIZE_BYTES);
 
+        // calculates coordinate length and offset based on image size
         let (coordinate_length, offset) =
             Coordinates::calc_coord_len_and_offset(self.header.image_size as u32);
 
-        match stream.read_buf(&mut buf).await {
+        // reads header by reading exactly HEADER_SIZE number of bytes
+        match client.read_buf(&mut header_buf).await {
             Ok(0) => {
                 return Err(DecodeError::Other(format!(
-                    "Error Reading from stream, read 0 bytes"
+                    "Error Reading from client, read 0 bytes"
                 )));
             }
             Ok(n) => {
-                if n != 11 {
+                if n != HEADER_SIZE_BYTES {
                     return Err(DecodeError::Other(format!(
                         "Length missmatch, expected headersize of 10, got {}",
                         n
                     )));
                 } else {
-                    self.decode_header(&buf);
-                    let mut payload_buf = BytesMut::with_capacity(self.header.length as usize);
-                    match stream.read_buf(&mut payload_buf).await {
-                        Ok(0) => {
-                            return Err(DecodeError::Other(format!(
-                                "Error Reading from stream, read 0 bytes"
-                            )));
-                        }
-                        Ok(n) => {
-                            if n != self.header.length as usize {
-                                return Err(DecodeError::Other(format!(
-                                    "Length missmatch, expected headersize of 10, got {}",
-                                    n
-                                )));
-                            } else {
-                                return Ok(self.decode_payload(&buf, coordinate_length, offset));
-                            }
-                        }
-                        Err(e) => {
-                            return Err(DecodeError::Other(format!(
-                                "Failed to read payload from port; err = {:?}",
-                                e
-                            )));
-                        }
-                    }
+                    // if HEADER_SIZE number of bytes have been read, then decode the header
+                    self.decode_header(&header_buf);
                 }
             }
             Err(e) => {
@@ -175,6 +160,42 @@ impl Packet {
                 )));
             }
         }
+
+        // initialse payload buffer to be read into
+        let mut payload_buf = BytesMut::with_capacity(self.header.length as usize);
+
+        let mut bytes_read = 0;
+        loop {
+            match client.read(&mut payload_buf[bytes_read..]).await {
+                Ok(0) => {
+                    return Err(DecodeError::Other(format!(
+                        "Error Reading from stream, read 0 bytes"
+                    )));
+                }
+                Ok(n) => {
+                    bytes_read += n;
+                    // if all the bytes have been read then break from the loop and process them
+                    if bytes_read >= self.header.length as usize {
+                        break;
+                    }
+                }
+                Err(e) => {
+                    return Err(DecodeError::Other(format!(
+                        "Failed to read payload from port; err = {:?}",
+                        e
+                    )));
+                }
+            }
+        }
+        // initialise IndexSet for cells
+        // initialising hear to decouple from decode_payload function for easy future modification
+        // is currently initialised after the stream to avoid allocation time in the case of failure as index set allocation takes O(n)
+        let mut cells = IndexSet::with_capacity(self.header.length as usize);
+
+        // decode the payload
+        self.decode_payload(&mut cells, &payload_buf, coordinate_length, offset);
+
+        Ok(cells)
     }
 
     pub fn encode_payload_from_set(
@@ -231,55 +252,101 @@ impl Packet {
         data
     }
 
-    pub fn encode_header(&self, payload_length: u32, fn_call_id: u8, sum: &mut u32) -> Vec<u8> {
+    /// Encodes header while calculating checksum.
+    /// Computed checksum here to avoid having to pass around and get out all this data in a separate function.
+    ///
+    /// Note that `message length` = `payload length` + `header length`
+    pub fn encode_header(&self, message_length: u32, fn_call_id: u8, sum: &mut u32) -> Vec<u8> {
         let mut data = Vec::with_capacity(HEADER_SIZE_BYTES);
 
         data.push(self.header.version);
         data.push(fn_call_id);
         data.push_u16_to_u8s(self.header.msg_id);
         data.push_u16_to_u8s(self.header.image_size);
-        data.push_u32_to_u8s(payload_length);
+        data.push_u32_to_u8s(message_length);
 
         self.ones_complement_sum(sum, self.header.version as u16);
         self.ones_complement_sum(sum, fn_call_id as u16);
         self.ones_complement_sum(sum, self.header.msg_id);
         self.ones_complement_sum(sum, self.header.image_size);
-        self.ones_complement_sum(sum, (payload_length >> 16) as u16);
-        self.ones_complement_sum(sum, payload_length as u16);
+        self.ones_complement_sum(sum, (message_length >> 16) as u16);
+        self.ones_complement_sum(sum, message_length as u16);
         return data;
     }
 
-    pub fn encode_cells_from_set(
+    /// Encodes cells to bytes and writes them to the stream  
+    ///
+    /// Use of `write_all` ensures all bytes are written or there is an error before the function returns.
+    pub async fn write_cells_from_set(
         &self,
-        client: &TcpStream,
+        client: &mut TcpStream,
         cells: IndexSet<u32>,
         function_call_id: u8,
-    ) {
+    ) -> Result<(), DecodeError> {
         let (coordinate_length, _) =
             Coordinates::calc_coord_len_and_offset(self.header.image_size as u32);
 
         // payload_length is length of alivecells in bytes + header size in bytes
         let payload = self.encode_payload_from_set(cells, coordinate_length as usize);
 
-        let payload_length = payload.len() as u32 + HEADER_SIZE_BYTES as u32;
+        let message_length = payload.len() as u32 + HEADER_SIZE_BYTES as u32;
         let mut sum: u32 = 0;
-        
-        let mut header = self.encode_header(payload_length, function_call_id, &mut sum);
 
+        let mut header = self.encode_header(message_length, function_call_id, &mut sum);
+
+        // sum up contents of payload
+        // could combine into encoding but doesnt increase runtime apart from increasing constant factor
+        // combining into encoding would just increase complexity for a reduction in the constant factor of only ≈1
         for i in 0..payload.len() / 2 {
             let word = ((payload[i * 2] as u16) << 8) | (payload[i * 2 + 1] as u16);
             self.ones_complement_sum(&mut sum, word);
         }
-        // Handle odd-length payload
+        // handle odd-length payload
         if payload.len() % 2 == 1 {
             self.ones_complement_sum(&mut sum, (payload[payload.len() - 1] as u16) << 8);
         }
 
+        // bitwise ones compliment
         let checksum = !(sum as u16);
+
+        // append checksum to last 2 bytes of header
         header.push_u16_to_u8s(checksum);
 
-        // write HEADER_SIZE number of bites
-        // write payload length number of bites
+        // WILL USE WHEN WORKING => USING ASSERT FOR NOW TO PANIC AND THROW ERROR RATHER THAN IMPLEMENT SPECIFIC HANDLING
+        // if header.len() != HEADER_SIZE_BYTES {
+        //     return Err(DecodeError::Other(format!(
+        //         "Header is the wrong length, should be {:?} bytes long but is {:?} bytes long instead. \n
+        //         The header is: {:?} \n",
+        //         HEADER_SIZE_BYTES, header.len(), header
+        //     )));
+        // }
+        assert_eq!(HEADER_SIZE_BYTES, header.len(), 
+                "Header is the wrong length, should be {:?} bytes long but is {:?} bytes long instead. \n
+                The header is: {:?} \n",
+                HEADER_SIZE_BYTES, header.len(), header
+            );
+
+        // write HEADER_SIZE number of bites returning error if there is one
+        match client.write_all(&header).await {
+            Ok(_) => {
+                // write payload length number of bites if header returns successfully
+                return match client.write_all(&payload).await {
+                    Ok(_) => Ok(()),
+                    Err(e) => {
+                        return Err(DecodeError::Other(format!(
+                            "Failed to write payload from port; err = {:?}",
+                            e
+                        )));
+                    }
+                };
+            }
+            Err(e) => {
+                return Err(DecodeError::Other(format!(
+                    "Failed to write header from port; err = {:?}",
+                    e
+                )));
+            }
+        };
     }
 
     /// generates mask of left aligned 1's where there are `coordinate_length` number of 1's
